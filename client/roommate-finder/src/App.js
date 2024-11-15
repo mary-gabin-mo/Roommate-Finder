b53e4fa7360afddeda6c8fad9e5fb6132fce54ae
jennydang61 <jennydang61@gmail.com>
2024-11-14 18:28:24 -0700

    made header

--- client/roommate-finder/src/App.js
+++ client/roommate-finder/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './landing/landingPage';
 import CreateAccount from './register/createAccount';
 import SignIn from './signin/signIn';
 import Home from './home/Home';
+import HeaderLayout from './header/headerLayout';
 
 function App() {
     return (
@@ -11,7 +12,7 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/createAccount" element={<CreateAccount />} />
                 <Route path="/signIn" element={<SignIn />} />
-                <Route path="/Home" element={<Home />} />
+                <Route path="/Home" element={ <HeaderLayout> <Home /></HeaderLayout>}/>
             </Routes>
         </Router>
 
