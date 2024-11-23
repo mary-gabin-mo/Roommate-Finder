d1cc26714a98da2966c15ffeffadceb491341fab
jennydang61 <jennydang61@gmail.com>
2024-11-23 14:20:18 -0700

    update profileRoutes

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/profile", async (req, res) => {
     const {
+        profile_ID,
         user_ID,
         description,
         rentRange,
@@ -17,50 +18,30 @@ router.post("/profile", async (req, res) => {
         sleepSchedule,
     } = req.body;
 
-    try {
-        const profileQuery = `INSERT INTO Profile (profile_ID, user_ID, description) VALUES (UUID(), ?, ?)`;
-        const [profileResult] = await connection.query(profileQuery, [user_ID, description]);
-        const profile_ID = profileResult.insertId; // auto-increment
+    const profileQuery = `INSERT INTO Profile (profile_ID, user_ID, description) VALUES (?, ?, ?)`;
+    const values = [profile_ID, user_ID, description]
 
-        const preferenceQuery = `INSERT INTO Preference (preference_ID, profile_ID, user_ID) VALUES (UUID(), ?, ?)`;
-        const [preferenceResult] = await connection.query(preferenceQuery, [profile_ID, user_ID]);
-        const preference_ID = preferenceResult.insertId; // auto-increment
+    const preferenceQuery = `INSERT INTO Preference (preference_ID, profile_ID, user_ID) VALUES (?, ?, ?)`;
 
-        const preferenceQueries = [
-            rentRange && {
-              query: "INSERT INTO RentRange (preference_ID, range) VALUES (?, ?)",
-              values: [preferenceId, rentRange],
-            },
-            cleanliness && {
-              query: "INSERT INTO Cleanliness (preference_ID, clean_level) VALUES (?, ?)",
-              values: [preferenceId, cleanliness],
-            },
-            roomCapacity && {
-              query: "INSERT INTO RoomCapacity (preference_ID, room_amount) VALUES (?, ?)",
-              values: [preferenceId, roomCapacity],
-            },
-            location && {
-              query: "INSERT INTO Location (preference_ID, quadrant) VALUES (?, ?)",
-              values: [preferenceId, location],
-            },
-            noiseTolerance && {
-              query: "INSERT INTO NoiseTolerance (preference_ID, tolerance_level) VALUES (?, ?)",
-              values: [preferenceId, noiseTolerance],
-            },
-            socialHabits && {
-              query: "INSERT INTO SocialHabits (preference_ID, habits) VALUES (?, ?)",
-              values: [preferenceId, socialHabits],
-            },
-            sleepSchedule && {
-              query: "INSERT INTO SleepSchedule (preference_ID, sleep_type) VALUES (?, ?)",
-              values: [preferenceId, sleepSchedule],
-            },
-          ];
+    const rentQuery = 'INSERT INTO RentRange (preference_ID, range) VALUES (?, ?)';
 
-        return res.status(200).json({ message: "Profile successfully created" });
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+    const cleanQuery = 'INSERT INTO Cleanliness (preference_ID, clean_level VALUES (?,?)';
+
+    const roomQuery = 'INSERT INTO RoomCapacity (preference_ID, room_amount VALUES (?,?)';
+
+    const locationQuery = 'INSERT INTO Location (preference_ID, quadrant) VALUES (?,?)';
+
+    const noiseQuery = 'INSERT INTO NoiseTolerance (preference_ID, tolerance_level) VALUES (?,?)';
+
+    const socialQuery = 'INSERT INTO SocialHabits (preference_ID, habits) VALUES (?,?)';
+
+    const sleepQuery = 'INSERT INTO SleepSchedule (preference_ID, sleep_type) VALUES (?,?)';
+
+    /*db.profileQuery(profileQuery, [values], (err, data) =>{
+      if (err) return res.json(err);
+      return res.json("Profile successfully created")
+    })*/
+
 });
 
 export default router;
